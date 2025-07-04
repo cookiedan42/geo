8fbdb1edfb4d135d8561df78803b3eac54f4d152
cookiedan42 <git@cookiedan42.com>
2025-07-04 15:50:55 +0800

    break out blanket linestring into macro

--- geo/src/algorithm/intersects/line_string.rs
+++ geo/src/algorithm/intersects/line_string.rs
@@ -2,20 +2,37 @@ use super::{has_disjoint_bboxes, Intersects};
 use crate::BoundingRect;
 use crate::*;
 
-// Blanket implementation using self.lines().any().
-impl<T, G> Intersects<G> for LineString<T>
-where
-    T: CoordNum,
-    Line<T>: Intersects<G>,
-    G: BoundingRect<T>,
-{
-    fn intersects(&self, geom: &G) -> bool {
-        if has_disjoint_bboxes(self, geom) {
-            return false;
+macro_rules! blanket_intersects_linestring {
+    ($t:ty) => {
+        impl<T> $crate::Intersects<$t> for LineString<T>
+        where
+            T: CoordNum,
+            Line<T>: Intersects<$t>,
+            $t: BoundingRect<T>,
+        {
+            fn intersects(&self, rhs: &$t) -> bool {
+                if has_disjoint_bboxes(self, rhs) {
+                return false;
+            }
+            self.lines().any(|l| l.intersects(rhs))
+            }
         }
-        self.lines().any(|l| l.intersects(geom))
-    }
+    };
 }
+
+blanket_intersects_linestring!(Coord<T>);
+blanket_intersects_linestring!(LineString<T>);
+blanket_intersects_linestring!(MultiLineString<T>);
+blanket_intersects_linestring!(Line<T>);
+blanket_intersects_linestring!(Point<T>);
+blanket_intersects_linestring!(MultiPoint<T>);
+blanket_intersects_linestring!(Polygon<T>);
+blanket_intersects_linestring!(MultiPolygon<T>);
+blanket_intersects_linestring!(Rect<T>);
+blanket_intersects_linestring!(Triangle<T>);
+blanket_intersects_linestring!(Geometry<T>);
+blanket_intersects_linestring!(GeometryCollection<T>);
+
 
 // Blanket implementation from LineString<T>
 impl<T, G> Intersects<G> for MultiLineString<T>
