315204158ef275e84d90a5da99c93f446215a0a7
cookiedan42 <git@cookiedan42.com>
2025-07-31 13:39:10 +0800

    multilinestring

--- geo/src/algorithm/covers/polygon.rs
+++ geo/src/algorithm/covers/polygon.rs
@@ -1,4 +1,5 @@
 use super::{impl_covers_from_intersects, impl_covers_from_relate, Covers};
+use crate::HasDimensions;
 use crate::{geometry::*, CoordsIter, Intersects};
 use crate::{GeoFloat, GeoNum};
 
@@ -13,10 +14,10 @@ where
 
 impl_covers_from_intersects!(Polygon<T>, [Point<T>, MultiPoint<T>]);
 impl_covers_from_relate!(Polygon<T>, [Line<T>]);
-impl_covers_from_relate!(Polygon<T>, [ LineString<T>,  MultiLineString<T>]);
-impl_covers_from_relate!(Polygon<T>, [ Rect<T>, Triangle<T>]);
+impl_covers_from_relate!(Polygon<T>, [LineString<T>,  MultiLineString<T>]);
+impl_covers_from_relate!(Polygon<T>, [Rect<T>, Triangle<T>]);
 impl_covers_from_relate!(Polygon<T>, [Polygon<T>,  MultiPolygon<T>]);
-impl_covers_from_relate!(Polygon<T>, [Geometry<T>, GeometryCollection<T>]);
+impl_covers_from_relate!(Polygon<T>, [GeometryCollection<T>]);
 
 impl<T> Covers<Coord<T>> for MultiPolygon<T>
 where
@@ -28,7 +29,7 @@ where
 }
 impl_covers_from_intersects!(MultiPolygon<T>, [Point<T>, MultiPoint<T>]);
 impl_covers_from_relate!(MultiPolygon<T>, [Line<T>]);
-impl_covers_from_relate!(MultiPolygon<T>, [ LineString<T>,  MultiLineString<T>]);
-impl_covers_from_relate!(MultiPolygon<T>, [ Rect<T>, Triangle<T>]);
+impl_covers_from_relate!(MultiPolygon<T>, [LineString<T>,  MultiLineString<T>]);
+impl_covers_from_relate!(MultiPolygon<T>, [Rect<T>, Triangle<T>]);
 impl_covers_from_relate!(MultiPolygon<T>, [Polygon<T>,  MultiPolygon<T>]);
-impl_covers_from_relate!(MultiPolygon<T>, [Geometry<T>, GeometryCollection<T>]);
+impl_covers_from_relate!(MultiPolygon<T>, [GeometryCollection<T>]);
