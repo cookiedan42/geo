fedd55f4c85a54bc728def98de19998b8dcbe3be
cookiedan42 <git@cookiedan42.com>
2025-10-03 11:21:02 +0800

    ugly version

--- geo/benches/contains_properly.rs
+++ geo/benches/contains_properly.rs
@@ -7,38 +7,61 @@ use geo_types::PointsIter;
 
 fn compare_poly_in_poly(c: &mut Criterion) {
     use geo::algorithm::{Contains, Relate};
-
-    c.bench_function("contains_properly", |bencher| {
         let poly1: Polygon<f64> =
             wkt! {POLYGON((9 0,9 9,0 9,0 0,9 0),(6 3,6 6,3 6,3 3,6 3))}.convert();
         let poly2: Polygon<f64> =
             wkt! {POLYGON((8 1,8 8,1 8,1 1,8 1),(7 2,7 7,2 7,2 2,7 2))}.convert();
+
+        let multipoly1 = MultiPolygon::new(vec![poly1.clone()]);
+        let multipoly2 = MultiPolygon::new(vec![poly2.clone()]);
+
+    c.bench_function("contains_properly poly poly", |bencher| {
+
 
         bencher.iter(|| {
             assert!(criterion::black_box(&poly1).contains_properly(criterion::black_box(&poly2)));
         });
     });
-    c.bench_function("contains", |bencher| {
-        let poly1: Polygon<f64> =
-            wkt! {POLYGON((9 0,9 9,0 9,0 0,9 0),(6 3,6 6,3 6,3 3,6 3))}.convert();
-        let poly2: Polygon<f64> =
-            wkt! {POLYGON((8 1,8 8,1 8,1 1,8 1),(7 2,7 7,2 7,2 2,7 2))}.convert();
+    c.bench_function("contains poly poly", |bencher| {
+      
 
         bencher.iter(|| {
             assert!(criterion::black_box(&poly1).contains(criterion::black_box(&poly2)));
         });
     });
 
-    c.bench_function("relate", |bencher| {
-        let poly1: Polygon<f64> =
-            wkt! {POLYGON((9 0,9 9,0 9,0 0,9 0),(6 3,6 6,3 6,3 3,6 3))}.convert();
-        let poly2: Polygon<f64> =
-            wkt! {POLYGON((8 1,8 8,1 8,1 1,8 1),(7 2,7 7,2 7,2 2,7 2))}.convert();
-
+    c.bench_function("relate poly poly", |bencher| {
+     
         bencher.iter(|| {
             assert!(
                 criterion::black_box(&poly1)
                     .relate(criterion::black_box(&poly2))
+                    .is_contains_properly()
+            );
+        });
+    });
+
+        c.bench_function("contains_properly multipoly multipoly", |bencher| {
+
+
+        bencher.iter(|| {
+            assert!(criterion::black_box(&multipoly1).contains_properly(criterion::black_box(&multipoly2)));
+        });
+    });
+    c.bench_function("contains multipoly multipoly", |bencher| {
+      
+
+        bencher.iter(|| {
+            assert!(criterion::black_box(&multipoly1).contains(criterion::black_box(&multipoly2)));
+        });
+    });
+
+    c.bench_function("relate multipoly multipoly", |bencher| {
+     
+        bencher.iter(|| {
+            assert!(
+                criterion::black_box(&multipoly1)
+                    .relate(criterion::black_box(&multipoly2))
                     .is_contains_properly()
             );
         });
--- geo/src/algorithm/contains_properly/polygon.rs
+++ geo/src/algorithm/contains_properly/polygon.rs
@@ -136,10 +136,122 @@ GeometryCollection<T>,
 Rect<T>,Triangle<T>
 ]);
 
+
+impl<T> ContainsProperly<Polygon<T>> for MultiPolygon<T>
+where
+    T: GeoNum,
+{
+    fn contains_properly(&self, rhs: &Polygon<T>) -> bool {
+        if self.is_empty() || rhs.is_empty() {
+            return false;
+        }
+
+        // no boundary intersection
+        if self
+            .lines_iter()
+            .any(|self_l| rhs.lines_iter().any(|rhs_l| self_l.intersects(&rhs_l)))
+        {
+            return false;
+        }
+        // all rings are concentric or disjoint
+
+        // if any point of rhs exterior lies within self.exterior, then all points of rhs exterior lie within self.exterior
+        let Some(rhs_ext_coord) = rhs.exterior().0.first() else {
+            return false;
+        };
+        // since no boundary intersection, rhs must be constrained within one of the polygons or totally disjoint
+        if self.0.iter().map(|poly|poly.exterior()).any(|ls|coord_pos_relative_to_ring(*rhs_ext_coord, ls) != CoordPos::Inside) {
+            return false;
+        }
+
+
+        // all holes of self must be covered by some hole of rhs
+        // if any hole of self is uncovered by a hole of rhs, then there exists a point of rhs which does not lie in self
+        // and hence return false
+        // since we know all rings are concentric or disjoint, we can just check the first point of each hole
+
+        for poly in self.0.iter() {
+            if coord_pos_relative_to_ring(*rhs_ext_coord, poly.exterior()) != CoordPos::Inside {
+                continue;
+            }
+
+            for self_hole in poly.interiors() {
+                // if self_hole is empty, then it is covered by rhs
+                let Some(self_hole_first_coord) = self_hole.0.first() else {
+                    continue;
+                };
+                if rhs.interiors().iter().any(|rhs_hole| {
+                    coord_pos_relative_to_ring(*self_hole_first_coord, rhs_hole) != CoordPos::Inside
+                }) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+impl<T> ContainsProperly<MultiPolygon<T>> for MultiPolygon<T>
+where
+    T: GeoNum,
+{
+    fn contains_properly(&self, rhs: &MultiPolygon<T>) -> bool {
+          if self.is_empty() || rhs.is_empty() {
+            return false;
+        }
+
+        // no boundary intersection
+        if self
+            .lines_iter()
+            .any(|self_l| rhs.lines_iter().any(|rhs_l| self_l.intersects(&rhs_l)))
+        {
+            return false;
+        }
+        // all rings are concentric or disjoint
+
+
+        for rhs_poly in rhs.0.iter() {
+             // if any point of rhs exterior lies within self.exterior, then all points of rhs exterior lie within self.exterior
+        let Some(rhs_ext_coord) = rhs_poly.exterior().0.first() else {
+            return false;
+        };
+        // since no boundary intersection, rhs must be constrained within one of the polygons or totally disjoint
+        if self.0.iter().map(|poly|poly.exterior()).any(|ls|coord_pos_relative_to_ring(*rhs_ext_coord, ls) != CoordPos::Inside) {
+            return false;
+        }
+
+
+        // all holes of self must be covered by some hole of rhs
+        // if any hole of self is uncovered by a hole of rhs, then there exists a point of rhs which does not lie in self
+        // and hence return false
+        // since we know all rings are concentric or disjoint, we can just check the first point of each hole
+
+        for poly in self.0.iter() {
+            if coord_pos_relative_to_ring(*rhs_ext_coord, poly.exterior()) != CoordPos::Inside {
+                continue;
+            }
+
+            for self_hole in poly.interiors() {
+                // if self_hole is empty, then it is covered by rhs
+                let Some(self_hole_first_coord) = self_hole.0.first() else {
+                    continue;
+                };
+                if rhs_poly.interiors().iter().any(|rhs_hole| {
+                    coord_pos_relative_to_ring(*self_hole_first_coord, rhs_hole) != CoordPos::Inside
+                }) {
+                    return false;
+                }
+            }
+        }
+        }
+        true
+
+
+    }
+}
+
 impl_contains_properly_from_relate!(MultiPolygon<T>, [
 Point<T>,MultiPoint<T>,
 Line<T>, LineString<T>, MultiLineString<T>,
-Polygon<T>,MultiPolygon<T>,
 GeometryCollection<T>,
 Rect<T>,Triangle<T>
 ]);
@@ -155,6 +267,16 @@ mod tests {
             wkt! {POLYGON((9 0,9 9,0 9,0 0,9 0),(6 3,6 6,3 6,3 3,6 3))}.convert();
         let poly2: Polygon<f64> =
             wkt! {POLYGON((8 1,8 8,1 8,1 1,8 1),(7 2,7 7,2 7,2 2,7 2))}.convert();
+
+        assert!(poly1.contains_properly(&poly2));
+    }
+
+       #[test]
+    fn test_contains_properly_donut_multi_multi() {
+        let poly1: MultiPolygon<f64> =
+            wkt! {MULTIPOLYGON(((9 0,9 9,0 9,0 0,9 0),(6 3,6 6,3 6,3 3,6 3)))}.convert();
+        let poly2: MultiPolygon<f64> =
+            wkt! {MULTIPOLYGON(((8 1,8 8,1 8,1 1,8 1),(7 2,7 7,2 7,2 2,7 2)))}.convert();
 
         assert!(poly1.contains_properly(&poly2));
     }
